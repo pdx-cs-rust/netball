7dd48a83d8f76902bd2e768fcb527c8e5b111b9c
Bart Massey <bart@cs.pdx.edu>
2020-02-26 17:56:40 -0800

    netball demo

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ansi_escapes::*;
 
 use std::convert::TryInto;
 use std::io::{stdout, Write};
+use std::net::*;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -9,21 +10,21 @@ const TICK: Duration = Duration::from_millis(100);
 
 const DIMS: (i16, i16) = (23, 80);
 
-fn main() {
+fn demo<W: Write>(mut out: W) {
     let mut x = 1;
     let mut y = 1;
     let mut dx = 1;
     let mut dy = 1;
     let mut stdout = stdout();
-    print!("{}{}", ClearScreen, CursorHide);
+    write!(out, "{}{}", ClearScreen, CursorHide).unwrap();
     stdout.flush().unwrap();
     loop {
         let cx = x.try_into().unwrap();
         let cy = y.try_into().unwrap();
-        print!("{}*", CursorTo::AbsoluteXY(cx, cy));
+        write!(out, "{}*", CursorTo::AbsoluteXY(cx, cy)).unwrap();
         stdout.flush().unwrap();
         sleep(TICK);
-        print!("{} ", CursorTo::AbsoluteXY(cx, cy));
+        write!(out, "{} ", CursorTo::AbsoluteXY(cx, cy)).unwrap();
         x += dx;
         y += dy;
         if x <= 0 || x >= DIMS.0 {
@@ -31,6 +32,23 @@ fn main() {
         }
         if y <= 0 || y >= DIMS.1 {
             dy = -dy;
+        }
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:13000").unwrap();
+    loop {
+        match listener.accept() {
+            Ok((socket, addr)) => {
+                println!("new client: {:?}", addr);
+                let _ = std::thread::spawn(move || {
+                    demo(socket);
+                });
+            }
+            Err(e) => {
+                println!("couldn't get client: {:?}", e);
+            }
         }
     }
 }
