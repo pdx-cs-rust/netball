0eaddfa667a597cf391ee58c13e48c60c6fafec8
Bart Massey <bart@cs.pdx.edu>
2020-02-26 18:33:05 -0800

    added synchronous multiclient service

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use ansi_escapes::*;
 
 use std::convert::TryInto;
-use std::io::{stdout, Write};
+use std::io::Write;
 use std::net::*;
+use std::sync::{Arc, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -10,21 +11,22 @@ const TICK: Duration = Duration::from_millis(100);
 
 const DIMS: (i16, i16) = (23, 80);
 
-fn demo<W: Write>(mut out: W) {
+fn demo(clients: Arc<Mutex<Vec<TcpStream>>>) {
     let mut x = 1;
     let mut y = 1;
     let mut dx = 1;
     let mut dy = 1;
-    let mut stdout = stdout();
-    write!(out, "{}{}", ClearScreen, CursorHide).unwrap();
-    stdout.flush().unwrap();
     loop {
         let cx = x.try_into().unwrap();
         let cy = y.try_into().unwrap();
-        write!(out, "{}*", CursorTo::AbsoluteXY(cx, cy)).unwrap();
-        stdout.flush().unwrap();
+        for out in clients.lock().unwrap().iter_mut() {
+            write!(out, "{}*", CursorTo::AbsoluteXY(cx, cy)).unwrap();
+            out.flush().unwrap();
+        }
         sleep(TICK);
-        write!(out, "{} ", CursorTo::AbsoluteXY(cx, cy)).unwrap();
+        for out in clients.lock().unwrap().iter_mut() {
+            write!(out, "{} ", CursorTo::AbsoluteXY(cx, cy)).unwrap();
+        }
         x += dx;
         y += dy;
         if x <= 0 || x >= DIMS.0 {
@@ -38,13 +40,18 @@ fn demo<W: Write>(mut out: W) {
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:13000").unwrap();
+    let clients = Arc::new(Mutex::new(Vec::new()));
+    let demo_clients = clients.clone();
+    let _ = std::thread::spawn(move || {
+        demo(demo_clients);
+    });
     loop {
         match listener.accept() {
-            Ok((socket, addr)) => {
+            Ok((mut socket, addr)) => {
                 println!("new client: {:?}", addr);
-                let _ = std::thread::spawn(move || {
-                    demo(socket);
-                });
+                write!(socket, "{}{}", ClearScreen, CursorHide).unwrap();
+                socket.flush().unwrap();
+                clients.lock().unwrap().push(socket);
             }
             Err(e) => {
                 println!("couldn't get client: {:?}", e);
